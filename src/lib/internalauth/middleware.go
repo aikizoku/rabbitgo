2bac6af8944c93d202e88458e6f3b996693932f5
aikizoku <>
2018-11-21 22:35:39 +0900

    key名を変更

--- src/lib/internalauth/middleware.go
+++ src/lib/internalauth/middleware.go
@@ -4,14 +4,14 @@ import "net/http"
 
 // Middleware ... 内部認証機能を提供するミドルウェア
 type Middleware struct {
-	AuthKey string
+	Token string
 }
 
 // Handle ... ハンドラ
 func (m *Middleware) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ah := r.Header.Get("Authorization")
-		if ah == "" || ah != m.AuthKey {
+		if ah == "" || ah != m.Token {
 			http.Error(w, "internal auth error.", http.StatusUnauthorized)
 			return
 		}
@@ -20,8 +20,8 @@ func (m *Middleware) Handle(next http.Handler) http.Handler {
 }
 
 // NewMiddleware ... Middlewareを作成する
-func NewMiddleware(authKey string) *Middleware {
+func NewMiddleware(token string) *Middleware {
 	return &Middleware{
-		AuthKey: authKey,
+		Token: token,
 	}
 }
