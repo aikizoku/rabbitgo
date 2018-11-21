15708534156b4d3b2ee1af83d5cdee8681fb375b
aikizoku <>
2018-11-21 23:26:52 +0900

    整理

--- src/lib/taskqueue/task.go
+++ src/lib/taskqueue/task.go
@@ -6,13 +6,23 @@ import (
 	"net/http"
 	"net/url"
 
-	"github.com/aikizoku/beego/src/lib/log"
+	"github.com/aikizoku/push/src/lib/internalauth"
+
+	"github.com/aikizoku/push/src/lib/log"
 	"google.golang.org/appengine/taskqueue"
 )
 
 // AddTask ... 通常リクエストをEnqueueする
 func AddTask(ctx context.Context, queue string, path string, params url.Values) error {
-	task := taskqueue.NewPOSTTask(queue, params)
+	h := make(http.Header)
+	h.Set("Content-Type", "application/x-www-form-urlencoded")
+	h.Set(internalauth.GetHeader())
+	task := &taskqueue.Task{
+		Path:    path,
+		Payload: []byte(params.Encode()),
+		Header:  h,
+		Method:  "POST",
+	}
 	return Add(ctx, queue, task)
 }
 
@@ -20,6 +30,7 @@ func AddTask(ctx context.Context, queue string, path string, params url.Values) 
 func AddTaskToJSON(ctx context.Context, queue string, path string, src interface{}) error {
 	h := make(http.Header)
 	h.Set("Content-Type", "application/json")
+	h.Set(internalauth.GetHeader())
 	data, err := json.Marshal(src)
 	if err != nil {
 		log.Errorf(ctx, "json.Marshal error: %s", err.Error())
