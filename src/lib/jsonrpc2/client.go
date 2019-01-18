c2cfd0ea79872775728844de551a3ff47e0e75e7
aikizoku <>
2019-01-18 10:34:38 +0900

    バグ修正

--- src/lib/jsonrpc2/client.go
+++ src/lib/jsonrpc2/client.go
@@ -18,24 +18,42 @@ type Client struct {
 }
 
 // AddRequest ... JSONRPC2のリクエストを登録する
-func (c *Client) AddRequest(id string, method string, params *json.RawMessage) {
+func (c *Client) AddRequest(ctx context.Context, id string, method string, params interface{}) error {
+	rawParams, err := c.marshalRawMessage(ctx, params)
+	if err != nil {
+		log.Errorm(ctx, "c.marshalRawMessage", err)
+		return err
+	}
 	c.Requests = append(c.Requests, &ClientRequest{
 		Version: version,
 		ID:      id,
 		Method:  method,
-		Params:  params,
+		Params:  rawParams,
 	})
+	return nil
 }
 
 // DoSingle ... JSONRPC2のシングルリクエストを行う
 func (c *Client) DoSingle(ctx context.Context, method string, params interface{}) (*json.RawMessage, *ErrorResponse, error) {
-	p, err := json.Marshal(params)
+	rawParams, err := c.marshalRawMessage(ctx, params)
+	if err != nil {
+		log.Errorm(ctx, "c.marshalRawMessage", err)
+		return nil, nil, err
+	}
+
+	req := &ClientRequest{
+		Version: version,
+		ID:      "single",
+		Method:  method,
+		Params:  rawParams,
+	}
+	rawReq, err := json.Marshal(req)
 	if err != nil {
 		log.Errorm(ctx, "json.Marshal", err)
 		return nil, nil, err
 	}
 
-	status, body, err := httpclient.PostJSON(ctx, c.URL, p, &httpclient.HTTPOption{Headers: c.Headers})
+	status, body, err := httpclient.PostJSON(ctx, c.URL, rawReq, &httpclient.HTTPOption{Headers: c.Headers})
 	if err != nil {
 		log.Errorm(ctx, "httpclient.PostJSON", err)
 		return nil, nil, err
@@ -83,6 +101,16 @@ func (c *Client) DoBatch(ctx context.Context) ([]*ClientResponse, error) {
 	}
 
 	return res, nil
+}
+
+func (c *Client) marshalRawMessage(ctx context.Context, params interface{}) (*json.RawMessage, error) {
+	bParams, err := json.Marshal(params)
+	if err != nil {
+		log.Errorm(ctx, "json.Marshal", err)
+		return nil, err
+	}
+	rawMessage := json.RawMessage(bParams)
+	return &rawMessage, nil
 }
 
 // NewClient ... Clientを作成する
