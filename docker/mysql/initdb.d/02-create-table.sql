63b9300a37e9facc8d94e32c90886f64f8a7eed0
aikizoku <>
2019-03-28 18:19:20 +0900

    oops

--- docker/mysql/initdb.d/02-create-table.sql
+++ docker/mysql/initdb.d/02-create-table.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS `sample`.`sample` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `enabled` tinyint(1) NOT NULL DEFAULT 1,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
+  `created_at` bigint(20) NOT NULL,
+  `updated_at` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 COMMENT 'sample';
@@ -16,8 +16,8 @@ COMMENT 'sample';
 --   `enabled` tinyint(1) NOT NULL DEFAULT 1,
 --   `dddd` enum('a', 'b', 'c', 'd') NOT NULL DEFAULT 'a',
 --   `notes` text,
---   `created_at` datetime NOT NULL,
---   `updated_at` datetime NOT NULL,
+--   `created_at` bigint(20) NOT NULL,
+--   `updated_at` bigint(20) NOT NULL,
 --   PRIMARY KEY (`id`),
 --   KEY idx_fuga_and_enabled (`fuga`, `enabled`)
 -- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
