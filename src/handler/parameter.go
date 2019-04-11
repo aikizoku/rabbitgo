6fb05b21b67ca597b120e72a1189dab578424f63
aikizoku <>
2019-04-11 16:07:41 +0900

    param取得を更新

--- src/handler/parameter.go
+++ src/handler/parameter.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
 	"mime/multipart"
 	"net/http"
+	"strconv"
 
 	"github.com/aikizoku/merlin/src/lib/log"
 	"github.com/go-chi/chi"
@@ -14,9 +16,31 @@ func GetURLParam(r *http.Request, key string) string {
 	return chi.URLParam(r, key)
 }
 
+// GetURLParamByInt64 ... リクエストからURLParamをint64で取得する
+func GetURLParamByInt64(ctx context.Context, r *http.Request, key string) (int64, error) {
+	str := chi.URLParam(r, key)
+	num, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		log.Warningm(ctx, "strconv.ParseInt", err)
+		return num, err
+	}
+	return num, nil
+}
+
 // GetFormValue ... リクエストからFormValueを取得する
 func GetFormValue(r *http.Request, key string) string {
 	return r.FormValue(key)
+}
+
+// GetFormValueByInt64 ... リクエストからFormValueをint64で取得する
+func GetFormValueByInt64(ctx context.Context, r *http.Request, key string) (int64, error) {
+	str := chi.URLParam(r, key)
+	num, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		log.Warningm(ctx, "strconv.ParseInt", err)
+		return num, err
+	}
+	return num, nil
 }
 
 // GetJSON ... リクエストからJSONを取得する
@@ -25,7 +49,7 @@ func GetJSON(r *http.Request, dst interface{}) error {
 	err := dec.Decode(dst)
 	if err != nil {
 		ctx := r.Context()
-		log.Errorm(ctx, "dec.Decode", err)
+		log.Warningm(ctx, "dec.Decode", err)
 		return err
 	}
 	return nil
