709a88f003e30e3db01ab0bfb523b97eebd21a04
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-08-02 16:10:20 +0900

    fix: add project id

--- enviroment.go
+++ enviroment.go
@@ -7,11 +7,11 @@ import (
 
 // Environment ... 環境変数
 type Environment struct {
-	Port            int    `envconfig:"PORT"                           default:"8080"`
-	Deploy          string `envconfig:"DEPLOY"                         required:"true"`
-	ProjectID       string `envconfig:"PROJECT_ID"                     required:"true"`
-	LocationID      string `envconfig:"LOCATION_ID"                    default:"asia-northeast1"`
-	ServiceID       string `envconfig:"SERVICE_ID"                     required:"true"`
+	Port      int    `envconfig:"PORT"                           default:"8080"`
+	Deploy    string `envconfig:"DEPLOY"                         required:"true"`
+	ProjectID string `envconfig:"PROJECT_ID"                     required:"true"`
+	// LocationID      string `envconfig:"LOCATION_ID"                    default:"asia-northeast1"`
+	// ServiceID       string `envconfig:"SERVICE_ID"                     required:"true"`
 	CredentialsPath string `envconfig:"GOOGLE_APPLICATION_CREDENTIALS" required:"true"`
 	MinLogSeverity  string `envconfig:"MIN_LOG_SEVERITY"               required:"true"`
 }
